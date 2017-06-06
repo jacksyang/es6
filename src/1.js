9f92de86008db223e9a97f46891adf21cb4bb2fc
jacksyang <513980580@qq.com>
2017-06-06 15:03:21 +0800

    123

--- src/1.js
+++ src/1.js
@@ -1,1 +1,19 @@
-let a=1;
+let a=2;
+
+function f1() {
+    let n=5;
+    if(true){
+        let n=10;
+    }
+    console.log(n);
+
+}
+function f2() {
+    let n=5;
+    if(true){
+         n=10;
+    }
+    console.log(n);
+
+}
+const yjz=123;
