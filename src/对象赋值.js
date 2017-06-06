898f938cc37bd7eccfc970e08d53a2ed40143859
jacksyang <513980580@qq.com>
2017-06-06 19:08:53 +0800

    23

--- src/对象赋值.js
+++ src/对象赋值.js
@@ -19,3 +19,19 @@ var {foo1,bar1} ={
 let a=1;
 //let [a]=[1];
 //一旦这么赋值以后 切记不可以用｛｝()再赋值；
+
+const [a3,b3]=[1,2]
+[a3,b4]=[b3,a3]
+
+class Animal {
+    //ES6中新型构造器
+    constructor(name) {
+        this.name = name;
+    }
+    //实例方法
+    sayName() {
+        console.log('My name is '+this.name);
+    }
+}
+Math.trunc(4,3);
+Math.sign(3)
