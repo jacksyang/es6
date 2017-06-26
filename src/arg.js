806b0475e5742b518637ab483d127e7097bc379f
jacksyang <513980580@qq.com>
2017-06-26 13:53:06 +0800

    123

---
+++ src/arg.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hdb on 2017/6/26.
+ */
+function yjz(...arg){
+    console.log(arg[0])
+    console.log(arg[1])
+    console.log(arg[2])
+    console.log(arg[3])
+    console.log(arg[4])
+
+}
+yjz(1,2,3)
---
+++ src/arr.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hdb on 2017/6/26.
+ */
+let arr1=["www","yjz","com"]
+let arr2=arr1;
+let arr3=[...arr1]
