48884a0fd98dc71231c824d8d2cfb00a8e240637
jacksyang <513980580@qq.com>
2018-01-25 11:17:13 +0800

    123

--- src/fun.js
+++ src/fun.js
@@ -3,3 +3,25 @@ function add(a,b=2){
 
 }
 console.log(add(1))
+
+var add1=(a,b=1)=>{
+    return a+b;
+}
+add1(1)
+
+
+let arr=["js","技术","前端"]
+var fun=(a,b,c)=>{
+    console.log(a,b,c)
+
+}
+fun(...arr)
+
+
+var a={a:1,b:2}
+console.log('a' in a);
+
+let arr4=[1,2,3,,,,]
+console.log(5 in arr4)//第一个这个是index，如果是空位返回false，
+
+//遍历数组 map,forEach,some,filter
